d62e77be2c2f6dba3a06f2941b548755f2059c33
Anshuman <anshuman.lalakers@gmail.com>
2022-05-03 22:00:22 -0700

    Updated codebase

--- background.js
+++ background.js
@@ -7,13 +7,15 @@ function sleep(ms) {
 
 function getHomepage(tab){
     console.log("Executing getHomepage function in background...");
-    chrome.tabs.executeScript(tab.id, {file: "get_homepage.js"});
+    //chrome.tabs.executeScript(tab.id, {file: "get_homepage.js"});
+    chrome.tabs.executeScript(null, {file: "get_homepage.js"});
 }
 
 
 function beginVideoCollection(tab) {
     console.log("Executing getVideos function in background...");
-    chrome.tabs.executeScript(tab.id, {file: "get_videos.js"});
+    //chrome.tabs.executeScript(tab.id, {file: "get_videos.js"});
+    chrome.tabs.executeScript(null, {file: "get_videos.js"});
 }
 
 function playVideo(vid_url) {
@@ -32,15 +34,6 @@ async function clickFunc(tab) {
     getHomepage(tab);
     await sleep(2*1000);
     beginVideoCollection(tab);
-    
-    //console.log(vids.slice());
-    //console.log(vids.length);
-    
-    //for (i = 0; i < vids.length; i++) {
-        //window.location.replace(vids[i]);
-        //playVideo(tab, vids[i]);
-        //await sleep(5*1000);
-    //}
 }
 
 
@@ -59,23 +52,47 @@ function urlToID(url){
 
 async function getSlant(video_url) {
    var v_id = urlToID(video_url);
-   const userAction = async () => {
-   const resp = await fetch('https://rostam.idav.ucdavis.edu/noyce/getSlant/' + String(v_id));
-   const resp_js = await resp.json();
-   var score = resp_js.slant;
-   return score;
-  }
+   console.log(v_id);
+   // Change the API later to rostam
+   let response = await fetch('http://www.randomnumberapi.com/api/v1.0/random?min=0&max=1&count=1')
+   return await response.json();
 }
 
 
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(async function(msg, sender, sendResponse) {
     var videos = msg;
     sendResponse("Got videos from injected script...");
     console.log(videos);
-    playAllVideos(videos, 5);
-    //for (i = 0; i < videos.length; i++) {
-    //    console.log(getSlant(videos[i]));
-    //}
+    //playAllVideos(videos, 5);
+
+    var scores_list = [];
+    var mean_score = 0;
+    for (i = 0; i < videos.length; i++) {
+        const js_resp = await getSlant(videos[i]);
+        // Add here ```var score = js_resp.slant``` and remove following line:
+        var score = js_resp[0];
+        console.log(score);
+        scores_list.push(score);
+        mean_score = mean_score + score;
+    }
+
+    mean_score = mean_score / videos.length;
+    console.log(mean_score);
+    var epsilon = 0.1; //Set threshold according to your need
+    var time2watch4 = 5;
+
+    if (Math.abs(mean_score - 0.0) > epsilon) {
+        // Read video to play from curated list here
+        var vid2play = 'https://www.youtube.com/watch?v=eB2OpurOFhk';
+        playVideo(vid2play);
+        await sleep(time2watch4 * 1000);
+
+    } else {
+        getHomepage(null);
+        return;
+    }
+
+    clickFunc(null);
 
 });
 
