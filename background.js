b81b03184e72c00bf2bd14a75f360edd88b6de9f
Anshuman <anshuman.lalakers@gmail.com>
2022-05-04 12:43:16 -0700

    Fixed bug

--- background.js
+++ background.js
@@ -7,29 +7,26 @@ function sleep(ms) {
 
 function getHomepage(tab){
     console.log("Executing getHomepage function in background...");
-    //chrome.tabs.executeScript(tab.id, {file: "get_homepage.js"});
-    chrome.tabs.executeScript(null, {file: "get_homepage.js"});
+    chrome.tabs.executeScript(tab.id, {file: "get_homepage.js"});
 }
 
 
 function beginVideoCollection(tab) {
     console.log("Executing getVideos function in background...");
-    //chrome.tabs.executeScript(tab.id, {file: "get_videos.js"});
-    chrome.tabs.executeScript(null, {file: "get_videos.js"});
+    chrome.tabs.executeScript(tab.id, {file: "get_videos.js"});
 }
 
-function playVideo(vid_url) {
-    url_json = { link : String(vid_url) };
-    chrome.tabs.executeScript(null, {
-    code: 'var video_obj = ' + JSON.stringify(url_json)}, function() {
-    chrome.tabs.executeScript(null, {file: 'play_video.js'});
-    });
 
+function playVideo(tab, vid_url) {
+    url_json = { link : String(vid_url) };
+    chrome.tabs.executeScript(tab.id, {
+    code: 'var video_obj = ' + JSON.stringify(url_json)}, function() {
+    chrome.tabs.executeScript(tab.id, {file: 'play_video.js'});
+    });
 }
 
 
 async function clickFunc(tab) {
-
     console.log("Executing main function..");
     getHomepage(tab);
     await sleep(2*1000);
@@ -44,17 +41,19 @@ async function playAllVideos(vids, seconds2watch) {
     }
 }
 
+
 function urlToID(url){
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
     var match = url.match(regExp);
     return (match&&match[7].length==11)? match[7] : false;
 }
 
+
 async function getSlant(video_url) {
    var v_id = urlToID(video_url);
    console.log(v_id);
-   // Change the API later to rostam
-   let response = await fetch('http://www.randomnumberapi.com/api/v1.0/random?min=0&max=1&count=1')
+   // Change the API later to rostam or use the file containing videos->slants
+   let response = await fetch('https://csrng.net/csrng/csrng.php?min=0&max=1')
    return await response.json();
 }
 
@@ -67,16 +66,20 @@ chrome.runtime.onMessage.addListener(async function(msg, sender, sendResponse) {
 
     var scores_list = [];
     var mean_score = 0;
+    var actual_count = 0
     for (i = 0; i < videos.length; i++) {
         const js_resp = await getSlant(videos[i]);
         // Add here ```var score = js_resp.slant``` and remove following line:
-        var score = js_resp[0];
+        var score = js_resp[0].random;
         console.log(score);
-        scores_list.push(score);
-        mean_score = mean_score + score;
+        if (score >= 0.0 && score <= 1.0) {
+            scores_list.push(score);
+            mean_score = mean_score + score;
+            actual_count = actual_count + 1;
+        }
     }
 
-    mean_score = mean_score / videos.length;
+    mean_score = mean_score / actual_count;
     console.log(mean_score);
     var epsilon = 0.1; //Set threshold according to your need
     var time2watch4 = 5;
@@ -84,15 +87,15 @@ chrome.runtime.onMessage.addListener(async function(msg, sender, sendResponse) {
     if (Math.abs(mean_score - 0.0) > epsilon) {
         // Read video to play from curated list here
         var vid2play = 'https://www.youtube.com/watch?v=eB2OpurOFhk';
-        playVideo(vid2play);
+        playVideo(sender.tab, vid2play);
         await sleep(time2watch4 * 1000);
 
     } else {
-        getHomepage(null);
+        getHomepage(sender.tab);
         return;
     }
 
-    clickFunc(null);
+    clickFunc(sender.tab);
 
 });
 
